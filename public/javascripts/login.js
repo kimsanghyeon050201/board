1817a89b8de74c82a615aa84463aca0c0a61e094
sanghyeonkim0502 <sanghyeonkim0502@gmail.com>
2023-10-06 10:56:26 +0900

    btn action test

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -3,18 +3,27 @@ const id = document.querySelector("#floatingInput")
 const pw = document.querySelector("#floatingInputPw")
 const btn = document.querySelector("#btn1")
 
-btn.addEventListener("click", ()=>{
-    const res = fetch("/login", {
-        method : "POST",
+btn.addEventListener("click", () => {
+    const res = fetch("http://localhost:3000/login", {
+        method: "POST",
         headers: {
-            "Content-Type":"application/json"
+            "Content-Type": "application/json",
+            'Accept': 'application/json'
         },
-        body : JSON.stringify({
+        body: JSON.stringify({
             'id': id.value,
-            'pw' : pw.value
+            'pw': pw.value
         })
-    }).then(reuslt => reuslt.json).then(reuslt => {
-        alert(reuslt)
+    }).then(result => {
+        return result.json()
+    }).then(result => {
+        const value = result.result
+        
+        if(value === undefined){
+            alert("실패")
+        }else{
+            alert("성공")
+        }
     })
 })
 
