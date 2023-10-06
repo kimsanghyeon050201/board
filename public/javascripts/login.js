2cee03d634afd82191e59ef55eaeb5e86c19c4c6
sanghyeonkim0502 <sanghyeonkim0502@gmail.com>
2023-10-06 11:15:01 +0900

    login success

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -4,7 +4,7 @@ const pw = document.querySelector("#floatingInputPw")
 const btn = document.querySelector("#btn1")
 
 btn.addEventListener("click", () => {
-    const res = fetch("http://localhost:3000/login", {
+    res = fetch("http://localhost:3000/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -22,7 +22,10 @@ btn.addEventListener("click", () => {
         if(value === undefined){
             alert("실패")
         }else{
+            console.log(value.name)
             alert("성공")
+            window.localStorage.setItem('name', value.name)
+            location.href=("http://localhost:3000/board")
         }
     })
 })
