e69d3ae1091b7949300679ed9685c2d2fabbe494
sanghyeonkim0502 <sanghyeonkim0502@gmail.com>
2023-10-10 10:41:03 +0900

    then edit

--- routes/index.js
+++ routes/index.js
@@ -24,28 +24,20 @@ router.post('/login', async (req, res) => {
       .input('id', sql.VarChar, id)
       .input('pw', sql.VarChar, pw)
       .query('select * from users where id = @id and pw = @pw')
-      .then((result) => {
-        const result_data = {
-          total: result.output.TOTAL,
-          result: result.recordset
-        }
-        return result_data
-      }).catch((err) => {
-        console.error(`err, ${err}`)
-        res.status(400).json({
-          message: "Fail",
-        })
-      })
-    if (Object.keys(result.result).length == 0) {
+    const result_data = {
+      result: result.recordset
+    }
+
+    if (result_data.result.length == 0) {
       res.status(400).json({
         message: "아이디 또는 비번 틀림"
       })
       return
     }
     res.status(200).json({
-      result: result.result[0]
+      result: result_data.result[0]
     })
-    console.log(result.result[0])
+    console.log(result_data.result[0])
   } catch (err) {
     res.status(400).json({
       message: "Fail",
@@ -60,20 +52,12 @@ router.get('/list', async (req, res) => {
 
     const result = await query.request()
       .query('select title, name, convert(varchar, la_time, 120) as la_time, views, id from post')
-      .then((result) => {
-        const result_data = {
-          total: result.output.TOTAL,
-          result: result.recordset
-        }
-        return result_data
-      }).catch((err) => {
-        console.error(`err, ${err}`)
-        res.status(400).json({
-          message: "Fail",
-        })
-      })
 
-    res.status(200).json(result)
+    const result_data = {
+      result: result.recordset
+    }
+
+    res.status(200).json(result_data)
   } catch (err) {
     console.error(`err, ${err}`)
     res.status(400).json({
@@ -92,16 +76,10 @@ router.patch('/edit', async (req, res) => {
       .input('id', sql.Int, parseInt(id))
       .input('title', sql.VarChar, title)
       .input('content', sql.VarChar, content)
-      .query('update post set title = @title, la_time = convert(varchar, GETDATE(), 120), content = @content where id = @id').then((result) => {
-        res.status(200).json({
-          message: "success"
-        })
-      }).catch((err) => {
-        console.error(`err, ${err}`)
-        res.status(400).json({
-          message: "Fail",
-        })
-      })
+      .query('update post set title = @title, la_time = convert(varchar, GETDATE(), 120), content = @content where id = @id')
+    res.status(200).json({
+      message: "success"
+    })
   } catch (err) {
     console.error(`err, ${err}`)
     res.status(400).json({
@@ -119,16 +97,10 @@ router.delete('/delete', async (req, res) => {
     const result = await query.request()
       .input('id', sql.Int, parseInt(id))
       .query('delete from post where id = @id')
-      .then(() => {
-        res.status(200).json({
-          message: "success"
-        })
-      }).catch((err) => {
-        console.error(`err, ${err}`)
-        res.status(400).json({
-          message: "Fail",
-        })
-      })
+
+    res.status(200).json({
+      message: "success"
+    })
   } catch (err) {
     console.error(`err, ${err}`)
     res.status(400).json({
@@ -149,15 +121,9 @@ router.post('/post', async (req, res) => {
       .input('title', sql.VarChar, title)
       .input('content', sql.VarChar, content)
       .query('INSERT into post(name, title, content, la_time, views) values(@name, @title, @content, convert(varchar, GETDATE(), 120), 0)')
-      .then(() => {
-        res.status(200).json({
-          message: "success"
-        })
-      }).catch((err) => {
-        console.error(`err, ${err}`)
-        res.status(400).json({
-          message: "Fail",
-        })
+      
+      res.status(200).json({
+        message: "success"
       })
   } catch (err) {
     console.error(`err, ${err}`)
